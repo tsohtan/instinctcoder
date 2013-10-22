d5497178dec08b6b62c53a105d98b8e9548964cf
tsohtan <tsohtan@instinctcoder.com>
2013-10-22 16:39:59 +0800

    MVC 4 Hello World Sample Code
    Sample code to learn  how the contoller and view communicate with each
    other

---
+++ 2013/10/HelloWorld/Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        // GET: /HelloWorld/
+
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Hello World!!!";
+
+            return View();
+        }
+
+    }
+}
